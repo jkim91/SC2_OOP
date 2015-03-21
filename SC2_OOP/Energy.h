1e6e339d0000942ba96fbcc990d64bec1c31fec3
Joon Kim <imjk91@gmail.com>
2015-03-21 15:06:12 -0400

    Changes to Energy.h

--- SC2_OOP/Energy.h
+++ SC2_OOP/Energy.h
@@ -4,11 +4,27 @@
 
 #include "Meter.h"
 
+class Regen;
+class GameObject;
+
 using namespace std;
 
-class Energy{
+class Energy : public Meter{
+public:
 	Energy();
 	Energy(Energy &e);
+	Energy(float &max, GameObject *g, float &rate, float &cooldown);
+	virtual ~Energy();
+
+	Regen* getRegen();
+	GameObject* getOwner();
+
+	void setRegen(Regen* r);
+	void setOwner(GameObject *owner);
+
+protected:
+	Regen *r;
+	GameObject *owner;
 };
 
 #endif
